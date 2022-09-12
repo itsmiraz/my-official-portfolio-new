fe68e54556d8db13ae6052570808cf4d00553178
itsmiraz <mirajulislamofficial044@gmail.com>
2022-09-12 15:16:00 +0600

    using local storage

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,60 +1,67 @@
 
+
+const modeChange = (para) => {
+
+    if (para === 'white') {
+        // Dark Mode 
+        const light = document.getElementById('light-mode');
+        light.style.display = 'block';
+
+        const night = document.getElementById('night-mode');
+        night.style.display = 'none';
+        document.body.style.backgroundColor = "white";
+
+        const elementcollections = document.getElementsByTagName('header');
+        for (const element of elementcollections) {
+            element.style.color = 'black';
+        }
+        const elementscollections = document.getElementsByTagName('section');
+        for (const element of elementscollections) {
+            element.style.color = 'black';
+        }
+        const elementdcollections = document.getElementsByTagName('p');
+        for (const element of elementdcollections) {
+            element.style.color = 'black';
+        }
+    }
+    else {
+        // Apply white theme 
+        const night = document.getElementById('night-mode');
+        night.style.display = 'block';
+        const light = document.getElementById('light-mode');
+        light.style.display = 'none';
+        document.body.style.backgroundColor = "#1f1f23";
+        const elementcollections = document.getElementsByTagName('header');
+        for (const element of elementcollections) {
+            element.style.color = 'white';
+        }
+        const elementscollections = document.getElementsByTagName('section');
+        for (const element of elementscollections) {
+            element.style.color = 'white';
+        }
+        const elementdcollections = document.getElementsByTagName('p');
+        for (const element of elementdcollections) {
+            element.style.color = 'white';
+        }
+
+
+    }
+
+}
+
 document.getElementById('night-mode').addEventListener('click', function () {
-    const light = document.getElementById('light-mode');
-    light.style.display = 'block';
+    modeChange('white')
 
-    const night = document.getElementById('night-mode');
-    night.style.display = 'none';
-    document.body.style.backgroundColor = "white";
-
-    const elementcollections = document.getElementsByTagName('header');
-    for (const element of elementcollections) {
-        element.style.color = 'black';
-    }
-    const elementscollections = document.getElementsByTagName('section');
-    for (const element of elementscollections) {
-        element.style.color = 'black';
-    }
-    const elementdcollections = document.getElementsByTagName('p');
-    for (const element of elementdcollections) {
-        element.style.color = 'black';
-    }
-
-
+    localStorage.setItem('Theme', 'white')
 })
 document.getElementById('light-mode').addEventListener('click', function () {
-    const night = document.getElementById('night-mode');
-
-    night.style.display = 'block';
-
-    const light = document.getElementById('light-mode');
-    light.style.display = 'none';
-
-    // Apply white theme 
-
-    document.body.style.backgroundColor = "#1f1f23";
-
-    const elementcollections = document.getElementsByTagName('header');
-    for (const element of elementcollections) {
-        element.style.color = 'white';
-    }
-    const elementscollections = document.getElementsByTagName('section');
-    for (const element of elementscollections) {
-        element.style.color = 'white';
-    }
-    const elementdcollections = document.getElementsByTagName('p');
-    for (const element of elementdcollections) {
-        element.style.color = 'white';
-    }
-
-
-
-
-
-
+    modeChange('dark')
+    localStorage.setItem('Theme', 'dark')
 })
 
 
 
+const loadLocalData = localStorage.getItem('Theme')
 
-// Contact
+// console.log(loadLocalData)
+modeChange(loadLocalData)
