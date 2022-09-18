c8ad876b6ff0e1cd03eca4ac2cb122af074115db
itsmiraz <mirajulislamofficial044@gmail.com>
2022-09-18 12:14:24 +0600

    tech i use section

---
+++ js/scroll-animation.js
@@ -0,0 +1,16 @@
+const ovserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+
+        }
+        else {
+            entry.target.classList.remove('show');
+        }
+    })
+})
+
+const hideElements = document.querySelectorAll('.hiden');
+
+hideElements.forEach((el) => ovserver.observe(el))
